29f5af9a3d619d05e63cf4ad66cf1f80ae10c511
Rui Wang <rw1@andrew.cmu.edu>
2015-10-06 13:37:21 -0400

    Update solution1_4 delete the extra blank lines
    Update solution1_4 delete the extra blank lines

--- ch1/solution_1_4.java
+++ ch1/solution_1_4.java
@@ -2,21 +2,17 @@ package CodingHomework1;
 
 public class solution_1_4 {
 	public static boolean PermutationOfPalindrome(String str){
-		
 		// assume the string ignore the space just care about the characters,ignore the upper or lower case of characters
 		//only input letters and space,no other character
-		
 		// solution idea:
 		// 1.ignore upper case letter and space
 		// 2.record each character's appearance times into an array
 		// 3.judge how many odd numbers in the array, when odd number more than 1,it is false,otherwise it is true
 		
-		
 		String str1 =  str.toLowerCase();   //ignore upper case
 		str1 = str1.replace(" ", "");       // remove space
 		
 		// 2.count each letter appearance time into an array 
-		
 		int[] a = new int[26];
 		for(int i=0;i<=str1.length()-1;i++){
 			char c = str1.charAt(i);  //pick value of position i 
@@ -25,7 +21,6 @@ public class solution_1_4 {
 		}
 		
 		//3. judge each value in a[26]
-		
 		int count = 0;
 		
 		for(int j=0;j<=a.length-1;j++){
@@ -40,7 +35,5 @@ public class solution_1_4 {
 			return false;
 		}
 			return true;
-
 	}
-	
 }
